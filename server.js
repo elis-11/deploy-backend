c517cb5160496eb2553d84071218bf832a4ca8ba
“elis-11” <elis-11>
2022-07-07 14:16:01 +0200

    Deploying

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const app = express()
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_ORIGIN,
     credentials: true,
   })
 ) // CORS check
